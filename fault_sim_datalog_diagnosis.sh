eb76e85ffcde0b599d10b52f47192da3837342ad
Ralibera <nanto.miaro@outlook.com>
2021-11-25 11:53:59 +0100

    Re-organization
    I put all the ressources in the new folder Files

--- fault_sim_datalog_diagnosis.sh
+++
@@ -1,9 +0,0 @@
-read_netlist CORE65GPSVT_tmax.v -library
-read_netlist c17_65n.v
-run_build_model c7552
-run_drc
-set_faults -model stuck
-add_faults -all
-set_patterns -external c7552.stil -sensitive
-run_simulation -pin { N1 1 } > datalog_SA1_N1.txt
-quit
